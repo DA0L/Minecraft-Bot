92cdb1aeaa144665a8f333f1a86be25b4ae1554b
DA0L <gabriol22@hotmail.com>
2021-01-21 20:09:37 +0100

    changed regex statement

--- kk.js
+++ kk.js
@@ -24,7 +24,7 @@ bot.on('message', function(msg) {
 */
 
 bot.on('chat', (guild, username, message) => {
-  console.log(message)
+  console.log(username)
   if (username === bot.username) return
   const command = message.split(" ")
   switch (true) {

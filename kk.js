970307efa074c58fa7592f907c49d651aee43681
DA0L <gabriol22@hotmail.com>
2021-01-21 14:12:29 +0100

    console.log

--- kk.js
+++ kk.js
@@ -50,6 +50,7 @@ function itemToString (item) {
 }
 
 bot.on('message', msg => console.log(msg.toString()))
+bot.on('message', msg => console.log(msg.toString().split("▶")[1]))
 
 function itemByName (name) {
   return bot.inventory.items().filter(item => item.name === name)[0]

8e90c659580571d2e9112e16d9b7e40c6d439c1f
DA0L <gabriol22@hotmail.com>
2021-01-21 20:23:50 +0100

    changed regex statement

--- kk.js
+++ kk.js
@@ -23,7 +23,7 @@ bot.on('message', function(msg) {
 })
 */
 
-bot.on('chat', (username, k, message) => {
+bot.on('chat', (guild, username, message) => {
   console.log(username)
   console.log(message)
   if (username === bot.username) return

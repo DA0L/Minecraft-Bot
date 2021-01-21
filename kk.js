9f8bcde2b3a2e34d334a7da5f678a945ff06832d
DA0L <gabriol22@hotmail.com>
2021-01-21 14:47:57 +0100

    sokit

--- kk.js
+++ kk.js
@@ -14,7 +14,7 @@ const bot = mineflayer.createBot({
 
 bot.on('message', function(msg) {
   const str = msg.toString()
-  const [completeMsg, username, message] = str.match(/【(.*))】(.*) ▶ (.*)/) || [str];
+  const [completeMsg, username, message] = str.match(/【(.*)】(.*) ▶ (.*)/) || [str];
   username = msg.toString()
   if (username && message) bot.emit("chat", username, message)
   console.log("jj")
